22c0a2d4ae8abb731ec44c79d199b5ab47b4f92d
LadySoulnight <48850997+LadySoulnight@users.noreply.github.com>
2019-04-08 22:06:26 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,7 @@ featureNames <- read.table("./UCI HAR Dataset/features.txt", colClasses = "chara
 selectedFeatures <- grep("-(mean|std)\\(", featureNames)
 
 # Extracts and reassembles the training and test data
-tidy_uci_har_dataset.csv <- data.frame(
+compact_uci_har_dataset <- data.frame(
   # Subjects
   rbind(
     read.table("./UCI HAR Dataset/test/subject_test.txt", colClasses = "factor"),
@@ -22,17 +22,17 @@ tidy_uci_har_dataset.csv <- data.frame(
 )
 
 # Renames the columns
-names(tidy_uci_har_dataset.csv) <- c(
+names(compact_uci_har_dataset) <- c(
   "Subject",
   featureNames[selectedFeatures],
   "Activity"
 )
 
 # Renames the activites
-levels(tidy_uci_har_dataset.csv$Activity) <- read.table("./UCI HAR Dataset/activity_labels.txt", colClasses = "character")[, 2]
+levels(compact_uci_har_dataset$Activity) <- read.table("./UCI HAR Dataset/activity_labels.txt", colClasses = "character")[, 2]
 
-# Stores the tidy dataset
-write.csv(tidy_uci_har_dataset.csv, file = "tidy_uci_har_dataset.csv")
+# Stores the compact dataset
+write.csv(compact_uci_har_dataset, file = "compact_uci_har_dataset.csv")
 
 #################################
 
@@ -43,9 +43,9 @@ if (!require(dplyr)) {
 }
 
 # Creates a summarised dataframe, averaging each feature for each activity and subject. 
-summarised_uci_har_dataset <- tidy_uci_har_dataset.csv %>%
+summarised_uci_har_dataset <- compact_uci_har_dataset %>%
   group_by(Subject, Activity) %>%
   summarise_all(mean)
 
-# Stores the summarised dataset
+# Stores the summarised (tidy) dataset
 write.csv(summarised_uci_har_dataset, file = "summarised_uci_har_dataset.csv")
